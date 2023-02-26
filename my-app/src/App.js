08eb93580683dca45bc32fac25c7a61f09be9d7f
Daniel Avshalumov <avshalumov.daniel@gmail.com>
2023-02-26 18:26:57 -0500

    profile template created

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import style from "./app.css"
+const App = () => {
+  return (
+    <main>
+      <div className="title">
+        <h1>Daniel Avshalumov's Profile</h1>
+      </div>
+      <hr />
+      <div className="projects">
+        <h3>Art Project - </h3>
+      </div>
+    </main>
+  );
+}
+
+export default App;
