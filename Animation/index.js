4ce3f1e9d10b1e9234b96c901d6be7850dd8c381
DanielAvshalumov <avshalumov.daniel@gmail.com>
2023-03-13 17:09:43 -0400

    new animations

--- Animation/index.js
+++ Animation/index.js
@@ -9,12 +9,14 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext('2d');
 
 function init() {
-    let x = move();
+    move();
     
 }
 let frame = 0;
 let frameCount = 0;
 let x = 0;
+let explosionDuration = 200;
+let flag = true;
 function move() {
     frameCount++;
     if(frameCount < 8) {
@@ -23,28 +25,38 @@ function move() {
     }
     frameCount = 0;
     const yPos = 90;
-    if(x > 150) {
+    if(x > 150 && explosionDuration > 0) {
         explode();
-        return x;
+        explosionDuration-=5;
+        console.log(x);
+    } else if(explosionDuration === 0 && x > 150){
+        x = 151;
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.drawImage(background,0,0);
+        ctx.drawImage(monke,0,yPos*frame,90,85,x,300,90,85);
+
     } else {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.drawImage(background,0,0);
-    ctx.drawImage(monke,0,yPos*frame,90,85,x+=5,300,90,85);
-    }
-    frame++;
+        
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.drawImage(background,0,0);
+        ctx.drawImage(monke,0,yPos*frame,90,85,x+=5,300,90,85);
+        frame++;
     if(frame >= 3) {
         frame = 0;
     }
     window.requestAnimationFrame(move);
+    }
+    
 }
 
 async function explode() {
     let explosion = new Image();
     let scaredMonke = new Image();
+    scaredMonke.src = "./scared-monke.png";
     explosion.src = "./explosion.png";
     const jsonFetch = await fetch("./explosion.json").then(res => res.json());
     let data = jsonFetch.frames;
-    console.log(data);
+    let req;
     let frameCount = 0;
     let frame = data.map(item => item.frame.y);
     let index = 0;
@@ -55,8 +67,8 @@ async function explode() {
     
     function helper() {
         frameCount++
-        if(frameCount < 10) {
-            window.requestAnimationFrame(helper);
+        if(frameCount < 25) {
+             req = window.requestAnimationFrame(helper);
             return;
         }
         frameCount = 0;
@@ -64,8 +76,10 @@ async function explode() {
         ctx.drawImage(background,0,0);
         if(index === 5) {
             index++
-            window.requestAnimationFrame(helper);
-        } 
+            req = window.requestAnimationFrame(helper);
+        }
+        
+        ctx.drawImage(scaredMonke,0,0,114,92,150,300,114,92);
         ctx.drawImage(explosion,0,frame[index],98,95,0,0,98,95);
         ctx.drawImage(explosion,0,frame[index],98,95,120,100,98,95);
         ctx.drawImage(explosion,0,frame[index],98,95,250,0,98,95);
@@ -76,7 +90,13 @@ async function explode() {
         if(index === 40) {
             index = 0;
         }
-        window.requestAnimationFrame(helper);
+        if(explosionDuration === 0) {
+            flag = false;
+            window.cancelAnimationFrame(req);
+        } else {
+            req = window.requestAnimationFrame(helper);
+        }
+
     }
 }
 
