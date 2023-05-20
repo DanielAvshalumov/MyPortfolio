46571aa002d969372bcf57b3ab8c3c822719580e
DanielAvshalumov <avshalumov.daniel@gmail.com>
2023-05-20 13:32:59 -0400

    bar graph added

--- DataViz/index.js
+++ DataViz/index.js
@@ -1,9 +1,9 @@
-var data = [
-  {name: "USA", value: 40},
+let data = [
+  {name: "Unites States", value: 40},
+  {name: "Italy", value: 30},
   {name: "UK", value: 20},
-  {name: "Canada", value: 30},
+  {name: "Russia", value: 15},
   {name: "Mexico", value: 10},
-  {name: "Russia", value: 15}
 ];
 
 const notesDiv = document.getElementById('reqs');
@@ -15,59 +15,118 @@ data.map(item => {
     notesDiv.append(element);
 })
 
-drawChart();
+drawDonoughtChart();
+drawBarChart();
 
-function drawChart() {
-    var text = "";
+function drawDonoughtChart() {
+    let text = "";
 
-    var width = 260;
-    var height = 260;
-    var thickness = 40;
-    var duration = 750;
+    let width = 260;
+    let height = 260;
+    let thickness = 40;
+    let duration = 750;
     
-    var radius = Math.min(width, height) / 2;
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    let radius = Math.min(width, height) / 2;
+    let color = d3.scaleOrdinal(d3.schemeCategory10);
     
-    var svg = d3.select("#chart")
+    let svg = d3.select("#chart")
     .append('svg')
     .attr('class', 'pie')
     .attr('width', width)
     .attr('height', height);
     
-    var g = svg.append('g')
+    let g = svg.append('g')
     .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')');
     
-    var arc = d3.arc()
+    let arc = d3.arc()
     .innerRadius(radius - thickness)
     .outerRadius(radius);
     
-    var pie = d3.pie()
+    let pie = d3.pie()
     .value(function(d) { return d.value; })
     .sort(null);
     
-    var path = g.selectAll('path')
+    let path = g.selectAll('path')
     .data(pie(data))
     .enter()
     .append("g")
-    
-      .append('path')
-      .attr('d', arc)
-      .attr('fill', (d,i) => color(i))
-      .on("mouseover", function(d) {
-          d3.select(this)     
-            .style("cursor", "pointer")
-            .style("fill", "black");
-        })
-      .on("mouseout", function(d) {
-          d3.select(this)
-            .style("cursor", "none")  
-            .style("fill", color(this._current));
-        })
-      .each(function(d, i) { this._current = i; });
-    
-    
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', (d,i) => color(i))
+    .each(function(d, i) { this._current = i; });
     g.append('text')
       .attr('text-anchor', 'middle')
       .attr('dy', '.35em')
       .text(text);
+}
+
+function drawBarChart() {
+    let data = {
+        "Stack Overflow": 30,
+        "Youtube"  : 44,
+        "Github"  : 64,
+        "Reddit" : 17,
+        "CrunchyRoll"   : 19
+    };
+    let margin = {top: 20, right: 20, bottom: 30, left: 40};
+    let svgWidth = 720, svgHeight = 300;
+    let height = svgHeight- margin.top- margin.bottom, width = svgWidth - margin.left - margin.right;
+    let sourceNames = [];
+    let sourceCount = [];
+    
+    let x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+        y = d3.scaleLinear().rangeRound([height, 0]);
+    for(let key in data){
+        if(data.hasOwnProperty(key)){
+            sourceNames.push(key);
+            sourceCount.push(parseInt(data[key]));
+        }
+    }
+    x.domain(sourceNames);
+    y.domain([0, d3.max(sourceCount, function(d) { return d; })]);
+    
+    let svg = d3.select("#bar-graph").append("svg");
+    svg.attr('height', svgHeight)
+        .attr('width', svgWidth);
+    
+    svg = svg.append("g")
+             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    svg.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+    
+    svg.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(5))
+        ;
+            
+    let bars = svg.selectAll('.bar')
+        .data(sourceNames)
+        .enter()
+        .append("g");
+    
+    bars.append('rect')
+        .attr('class', 'bar')
+        .attr("x", function(d) { return x(d); })
+        .attr("y", function(d) { return y(data[d]); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(data[d]); });
+        
+    bars.append("text")
+        .text(function(d) { 
+            return data[d];
+        })
+        .attr("x", function(d){
+            return x(d) + x.bandwidth()/2;
+        })
+        .attr("y", function(d){
+            return y(data[d]) - 5;
+        })
+        .attr("font-family" , "sans-serif")
+        .attr("font-size" , "14px")
+        .attr("fill" , "black")
+        .attr("text-anchor", "middle");
+        
 }
